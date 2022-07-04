bc16ced6453e46fb7573d6011bb2fd362d93c1c4
Malinda <malinda.dilhara@gmail.com>
2022-07-04 03:00:43 -0700

    use np.mean

--- theanoTUT/theano13_save/full_code.py
+++ theanoTUT/theano13_save/full_code.py
@@ -15,7 +15,7 @@ import pickle
 
 def compute_accuracy(y_target, y_predict):
     correct_prediction = np.equal(y_predict, y_target)
-    accuracy = np.sum(correct_prediction)/len(correct_prediction)
+    accuracy = np.mean(correct_prediction)
     return accuracy
 
 rng = np.random
